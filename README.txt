4890bd1ace05d87d4f38d5dc3db968545365bd40
dj51234 <dj51234@yahoo.com>
2016-07-27 17:43:47 -0400

    initial

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/dj51234/pen/jAKxRO.
+
+ 
---
+++ js/index.js
@@ -0,0 +1,4 @@
+$('.submit').on('click',function(){
+  $('.belt').toggleClass('transitioned');
+  $('.state-1').toggleClass('transitioned');
+});
